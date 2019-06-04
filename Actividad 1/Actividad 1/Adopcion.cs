514eb7507a982103820c318528e990baaee9f8f7
Rito12478 <50603192+Rito12478@users.noreply.github.com>
2019-06-03 20:41:34 -0500

    Update Adopcion.cs

--- Actividad 1/Actividad 1/Adopcion.cs
+++ Actividad 1/Actividad 1/Adopcion.cs
@@ -3,16 +3,17 @@ namespace Actividad_1
     class Adopcion
     {
 
-        private string IdUsuario;
-        public Usuario usuario;
-        public Mascota mascota;
+        private string IdAdopcion;
+        public Usuario _usuario;
+        public Mascota _mascota;
         private string NombreMascota;
         private string Fecha;
         private string Sucursal;
 
         public void Transaccion(Mascota mascota, Usuario usuario){
+            this._usuario = usuario;
+            this._mascota =  mascota;
         }
-        private void EntregarMascota(Mascota m){
-        }
+        
     }
 }
